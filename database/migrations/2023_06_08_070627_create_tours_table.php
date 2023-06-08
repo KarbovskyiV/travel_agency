43bbf9857bf3fb13e24fe7a59aad9b7dc4bc3430
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-06-08 13:13:12 +0300

    refactor to column name id, and to constrained

--- database/migrations/2023_06_08_070627_create_tours_table.php
+++ database/migrations/2023_06_08_070627_create_tours_table.php
@@ -4,16 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('tours', function (Blueprint $table) {
-            $table->uuid()->primary();
-            $table->foreignId('travel_id');
+            $table->uuid('id')->primary();
+            $table->foreignUuId('travel_id')->constrained('travels');
             $table->string('name');
             $table->date('starting_date');
             $table->date('ending_date');
