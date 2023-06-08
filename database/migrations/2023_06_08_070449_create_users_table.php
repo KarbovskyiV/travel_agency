c8342874e40186ceed693f4614cbf7f872f689da
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-06-08 13:11:42 +0300

    refactor to column name id

--- database/migrations/2023_06_08_070449_create_users_table.php
+++ database/migrations/2023_06_08_070449_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
--- database/migrations/2023_06_08_070520_create_roles_table.php
+++ database/migrations/2023_06_08_070520_create_roles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->timestamps();
         });
