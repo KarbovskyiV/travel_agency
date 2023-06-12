3004fdfa04cad0c00b14bd9189254ed5ec3c176a
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-06-12 13:38:37 +0300

    refactor an anonymous function

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -12,18 +12,14 @@ class TourController extends Controller
     public function index(Travel $travel, ToursListRequest $request)
     {
         $tours = $travel->tours()
-            ->when($request->priceFrom, function ($query) use ($request) {
-                $query->where('price', '>=', $request->priceFrom * 100);
-            })->when($request->priceTo, function ($query) use ($request) {
-                $query->where('price', '<=', $request->priceTo * 100);
-            })
-            ->when($request->dateFrom, function ($query) use ($request) {
-                $query->where('starting_date', '>=', $request->dateFrom);
-            })->when($request->dateTo, function ($query) use ($request) {
-                $query->where('starting_date', '<=', $request->dateTo);
-            })->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
-                $query->orderBy($request->sortBy, $request->sortOrder);
-            })
+            ->when($request->priceFrom, fn($query) => $query->where('price', '>=', $request->priceFrom * 100))
+            ->when($request->priceTo, fn($query) => $query->where('price', '<=', $request->priceTo * 100))
+            ->when($request->dateFrom, fn($query) => $query->where('starting_date', '>=', $request->dateFrom))
+            ->when($request->dateTo, fn($query) => $query->where('starting_date', '<=', $request->dateTo))
+            ->when(
+                $request->sortBy && $request->sortOrder,
+                fn($query) => $query->orderBy($request->sortBy, $request->sortOrder)
+            )
             ->orderBy('starting_date')
             ->paginate();
 
